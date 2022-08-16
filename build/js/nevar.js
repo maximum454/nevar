3456db3433b22d5d01b2483c51d27b84398ac44a
Maxim Karachyov <karachyov.m@gmail.com>
2022-08-16 17:08:58 +0300

    index

--- build/js/nevar.js
+++ build/js/nevar.js
@@ -1404,5 +1404,17 @@
 }, window.jQuery, window.Zepto));
 
 
+const swiperMain = new Swiper(".swiper-main", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    initialSlide: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
 
 
--- src/js/nevar.js
+++ src/js/nevar.js
@@ -2,5 +2,17 @@
 @@include('./partials/remodal.js')
 @@include('./partials/jquery.mask.js')
 
+const swiperMain = new Swiper(".swiper-main", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    initialSlide: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
 
 
